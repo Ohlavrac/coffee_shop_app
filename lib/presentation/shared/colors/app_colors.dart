61eabde3541a472535a4d141f55f761ab488f6ed
ohlavrac <joaovicrcarvalho@gmail.com>
2023-12-11 10:20:53 -0300

    feat: add app colors

---
+++ lib/presentation/shared/colors/app_colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const brandColor = Color(0xFF5D4037);
+  static const sucessColor = Color(0xFF43936C);
+  static const infoColor = Color(0xFF03539E);
+  static const dangerColor = Color(0xFFE33131);
+  static const warningColor = Color(0xFFEB9525);
+
+  static const lightColor = Color(0xFFFEFEFE);
+  static const mediumColor = Color(0xFFF0F0F0);
+  static const darkColor = Color(0xFF3C3C3C);
+
+  static const headingTextColor = Color(0xFF3C3C3C);
+  static const paragraphTextColor = Color(0xFF555555);
+  static const nonActiveTextColor = Color(0xFF868686);
+  static const disabledTextColor = Color(0xFFCACACA);
+
+  static const fifityBrandColor = Color(0xFFEFEBE9);
+  static const hundredBrandColor = Color(0xFFD7CCC8);
+  static const twoHundredBrandColor = Color(0xFFBCAAA4);
+  static const treehundredBrandColor = Color(0xFFA1887F);
+  static const fourhundredBrandColor = Color(0xFF8D6E63);
+  static const fivehundredBrandColor = Color(0xFF795548);
+  static const sixhundredBrandColor = Color(0xFF6D4C41);
+  static const mainBrandColor = Color(0xFF5D4037);
+  static const eighthundredBrandColor = Color(0xFF4E342E);
+  static const ninehundredBrandColor = Color(0xFF3E2723);
+
+  static const fifitySecondaryColor = Color(0xFFE6EFF3);
+  static const hundredSecondaryColor = Color(0xFFC7D6DB);
+  static const twoHundredSecondaryColor = Color(0xFFA6BBC1);
+  static const treehundredSecondaryColor = Color(0xFF839FA8);
+  static const fourhundredSecondaryColor = Color(0xFF6A8A95);
+  static const fivehundredSecondaryColor = Color(0xFF507783);
+  static const sixhundredSecondaryColor = Color(0xFF456872);
+  static const mainSecondaryColor = Color(0xFF37545D);
+  static const eighthundredSecondaryColor = Color(0xFF2A4149);
+  static const ninehundredSecondaryColor = Color(0xFF1A2C32);
+}

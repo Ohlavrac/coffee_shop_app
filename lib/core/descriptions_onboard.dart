51008a5437173bc17c4af3d09bf11ec1ba27e82e
ohlavrac <joaovicrcarvalho@gmail.com>
2023-12-11 15:12:43 -0300

    feat: create list of descriptions on board

---
+++ lib/core/descriptions_onboard.dart
@@ -0,0 +1,5 @@
+List<String> descriptionsOnboard = [
+  "Customize your own drink exactly how you like it by adding any topping you like!!!",
+  "You can place your order quickly and easly without wasting time. You can also schedule orders via your smarthphone.",
+  "Exciting prizes await you! Redeem yours by collecting all the points after purchase in the app!"
+];

7421ea15661986948fffe61ad0898ca564386d74
ohlavrac <joaovicrcarvalho@gmail.com>
2023-12-11 14:09:05 -0300

    fix: fix app texts weight

--- lib/presentation/shared/texts/app_texts.dart
+++ lib/presentation/shared/texts/app_texts.dart
@@ -3,70 +3,70 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTexts {
   static final body10Regular =
-      GoogleFonts.poppins(fontSize: 10, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 10, fontWeight: FontWeight.w400);
 
   static final body10Medium =
-      GoogleFonts.poppins(fontSize: 10, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 10, fontWeight: FontWeight.w500);
 
   static final body10Bold = GoogleFonts.poppins(
     fontSize: 10, fontWeight: FontWeight.bold
   );
 
   static final body12Regular =
-      GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400);
 
   static final body12Medium =
-      GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w500);
 
   static final body12Bold = GoogleFonts.poppins(
     fontSize: 12, fontWeight: FontWeight.bold
   );
 
   static final body14Regular =
-      GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400);
 
   static final body14Medium =
-      GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w500);
 
   static final body14Bold = GoogleFonts.poppins(
     fontSize: 14, fontWeight: FontWeight.bold
   );
 
   static final title16Regular =
-      GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400);
 
   static final title16Medium =
-      GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500);
 
   static final title16Bold = GoogleFonts.poppins(
     fontSize: 16, fontWeight: FontWeight.bold
   );
 
   static final title18Regular =
-      GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w400);
 
   static final title18Medium =
-      GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w500);
 
   static final title18Bold = GoogleFonts.poppins(
     fontSize: 18, fontWeight: FontWeight.bold
   );
 
   static final heading20Regular =
-      GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400);
 
   static final heading20Medium =
-      GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w500);
 
   static final heading20Bold = GoogleFonts.poppins(
     fontSize: 20, fontWeight: FontWeight.bold
   );
 
   static final heading24Regular =
-      GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w500);
+      GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w400);
 
   static final heading24Medium =
-      GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w700);
+      GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w500);
 
   static final heading24Bold = GoogleFonts.poppins(
     fontSize: 24, fontWeight: FontWeight.bold

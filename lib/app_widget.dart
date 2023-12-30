118065bb474b0233434c1d13f225024869364cdb
ohlavrac <joaovicrcarvalho@gmail.com>
2023-12-29 21:15:06 -0300

    feat: add routes to new pages on app widgets

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
+import 'package:coffee_shop_app/presentation/screens/login_screen/login_screen.dart';
 import 'package:coffee_shop_app/presentation/screens/onboarding_screen/onboarding_screen.dart';
+import 'package:coffee_shop_app/presentation/screens/register_screen/register_screen.dart';
 import 'package:coffee_shop_app/presentation/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,10 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/onboardscreen",
+      initialRoute: "/loginscreen",
       routes: {
         "/splashscreen": (context) => const SplashScreen(),
-        "/onboardscreen": (context) => const OnBoardingScreen()
+        "/onboardscreen": (context) => const OnBoardingScreen(),
+        "/registerscreen": (context) => const RegisterScreen(),
+        "/loginscreen": (context) => const LoginScreen()
       },
     );
   }
